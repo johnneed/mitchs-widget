b725fe88b9975ae5de9329a88c2a1af0fbc0c559
John Need <john.need@gmail.com>
2016-07-03 01:35:51 -0400

    readme

--- README.md
+++ README.md
@@ -1,5 +1,96 @@
 # mitchs-widget
 Mitch's Widget
 
-
+## To Run:
 Open mitches-widget.html in a browser.
+
+## To Use:
+load the script
+<script src="mitchs-widget.js"></script>
+
+
+## Create a new widget
+  var newWidget =  MITCH.mitchsWidget.create({
+            elementId: "mitchsWidget",
+            clickFunction: getMoreInfo,
+            titleTemplate:
+            buttonData: data // optional
+        });
+
+####elementId :: string
+The id the element you contain the new widget
+
+####clickFunction :: function
+The function you want called when a button is clicked.
+the analysisID will be passed as a parameter to this function
+
+####titleTemplate :: string - optional
+A string representing what will be placed in each button's title attibute
+surround key names encased in double braces will be replaced with their data values
+keys that don't match the data will be removed.
+
+this is the default title:
+"Area:{{area}},  Factor:{{factor}},  Product:{{product}}"
+
+#### buttonData :: Object - optional
+An array of data objects with the following schema:
+
+{
+        name::string,
+        id:string,
+        area:string [optional],
+        factor::string [optional],
+        product::string  [optional],
+    }
+
+
+example :
+
+[{
+        name: "Analysis 3",
+        id: "ABCD3",
+        area: "Area 1",
+        factor: "Factor 2",
+        product: "Product 1"
+  }, {
+         name: "Analysis 3",
+         id: "ABCD3",
+         area: "Area 1",
+         factor: "Factor 2",
+         product: "Product 1"
+}]
+
+If data is provided the widget will render upon creation.
+
+
+### Rendering
+To render new buttons pass a new data object to the render method
+
+var newData = [{
+        name: "Analysis 44",
+        id: "ABCD44",
+        area: "Area 1",
+        factor: "Factor 2",
+        product: "Product 1"
+   }, {
+         name: "Analysis 55",
+         id: "ABCD55",
+         area: "Area 1",
+         factor: "Factor 2",
+         product: "Product 1"
+   }];
+
+newWidget.render(newData);
+
+### Destroying
+Before unloading the DOM element containing the widget,
+you should first destroy the widget to avoid memory leaks,
+
+newWidget.destroy();
+
+
+
+
+
+
+
--- mitchs-widget.js
+++ mitchs-widget.js
@@ -1,39 +1,116 @@
 window.MITCH = window.MITCH || {};
-window.MITCH.mitchsWidget = function (data, clickFunc) {
+window.MITCH.mitchsWidget = (function () {
 
-    var template = "Area:{{area}}, " +
+
+    var defaultTitleTemplate = "Area:{{area}}, " +
         "Factor:{{factor}}, " +
         "Product:{{product}}";
 
-    var mitchsWidget = document.getElementById('mitchsWidget');
+    /**
+     * creates a new widget.
+     * @param {object} initObject
+     * initObject has this schema {elementId:string, clickFunction:function, [titleTemplate:string], [buttonData:object]}
+     * @returns {object} new Widget
+     */
+    function create(initObject) {
+        var clickFunction, buttonData, elementId, titleTemplate, parentElement;
 
-    function handleClick(id) {
-        clickFunc(id);
-    }
+        function handleClick(event){
+            var analysisId;
+            if(event && event.path) {
+                analysisId = event.path.reduce(function (id, elem) {
+                    return id || elem.getAttribute("data-id");
+                }, null);
+                if (analysisId !== null) {
+                    clickFunction(analysisId);
+                }
+            }
+        }
 
-    function createButtons(data) {
-        return data.reduce(function (buttons, analysis) {
-            var title = template;
-            var newButton = document.createElement("button");
-            newButton.className = "mitchs-widget_analysis";
-            newButton.onclick = handleClick.bind(null, analysis.id);
 
-            Object.keys(analysis).forEach(function (key) {
-                title = title.replace("{{" + key + "}}", analysis[key]);
+        function createButtons(data) {
+            var wrapper = document.createElement("div");
+            wrapper.className="mitchs-widget";
+
+            //Add buttons to our wrapper
+            data.forEach(function (analysis) {
+                var attr = document.createAttribute("data-id");
+                var title = titleTemplate;
+                var newButton = document.createElement("button");
+                attr.value = analysis.id;
+                newButton.className = "mitchs-widget_analysis";
+                newButton.setAttributeNode(attr);
+                Object.keys(analysis).forEach(function (key) {
+                    title = title.replace("{{" + key + "}}", analysis[key]);
+                });
+                title = title.replace(/{{.*?}}/g, "");
+                newButton.title = title;
+                newButton.innerHTML = "<span>" + analysis.name + "</span>";
+                wrapper.appendChild(newButton);
             });
-            title = title.replace(/{{.*?}}/g, "");
-            newButton.title = title;
-            newButton.innerHTML = "<span>" + analysis.name + "</span>";
-            buttons.push(newButton);
-            return buttons;
-        }, []);
+
+
+            return wrapper;
+        }
+
+        /**
+         *
+         * @param {object} [newButtonData] - array of analysis data.
+         * @returns {boolean}
+         */
+        function render(newButtonData) {
+            //Swap new data if we have new, valid data - otherwise we'll re-render the old data.
+            buttonData = (typeof newButtonData === "object" && newButtonData) || buttonData;
+            //Don't render if we don't have data
+            if (!buttonData) {
+                return false;
+            }
+            try {
+                parentElement.innerHTML = "";
+                parentElement.appendChild(createButtons(buttonData));
+                return true;
+            } catch(err){
+                return false;
+            }
+        }
+
+        function destroy() {
+            parentElement.removeEventListener("click", handleClick);
+            parentElement.innerHTML = "";
+        }
+
+
+
+        //sanity check
+        if (typeof initObject !== "object" || typeof initObject.clickFunction !== "function" || typeof initObject.elementId !== "string") {
+            throw Error("mitchsWidget requires initialization parameters elementId and clickFunction");
+        }
+
+        //Initialize our closure variables
+        clickFunction = initObject.clickFunction;
+        elementId = initObject.elementId;
+        titleTemplate = initObject.titleTemplate || defaultTitleTemplate;
+        buttonData = initObject.buttonData || {};
+        parentElement = document.getElementById(elementId);
+
+        //Check if we actually have DOM element to attach to.
+        if (!parentElement) {
+            return undefined;
+        }
+        //attach clickFunction
+        parentElement.addEventListener("click", handleClick);
+
+        //Render buttons if we got data.
+        render();
+
+
+        return {
+            render: render,
+            destroy: destroy
+        }
+
     }
 
-    function renderButtons(buttons) {
-        buttons.forEach(function (b) {
-            mitchsWidget.appendChild(b);
-        });
-    }
 
-    renderButtons(createButtons(data));
-};
+    return {create: create};
+}());
